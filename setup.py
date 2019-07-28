bb98992213fa4ba49ca7e08d92c4d21271b58ff2
Igor Kasianov <super.hang.glider@gmail.com>
2019-07-28 22:02:51 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'graphene-federation',
+  packages = ['graphene_federation'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'Federation implementation for graphene',
+  author = 'Igor Kasianov',
+  author_email = 'super.hang.glider@gmail.com',
+  url = 'https://github.com/erebus1/graphene-federation',
+  download_url = 'https://github.com/erebus1/graphene-federation/archive/v0.0.1.tar.gz',
+  keywords = ['graphene', 'gql', 'federation'],
+  install_requires=[
+          "graphene>=2.1.0,<3"
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Libraries",
+    "Programming Language :: Python :: 3.6",
+  ],
+)
