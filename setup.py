52670b8d88f0dddcc3f76ae327b0c73800b2f671
Igor Kasianov <super.hang.glider@gmail.com>
2019-08-12 03:17:31 +0300

    add long_description in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
 setup(
   name = 'graphene-federation',
   packages = ['graphene_federation'],
   version = '0.0.1',
   license='MIT',
   description = 'Federation implementation for graphene',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'Igor Kasianov',
   author_email = 'super.hang.glider@gmail.com',
   url = 'https://github.com/erebus1/graphene-federation',
