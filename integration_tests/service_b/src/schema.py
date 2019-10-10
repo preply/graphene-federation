0dcaead169bdc739829d707e159e79863adcf6a2
Jakub Draganek <jakub.draganek@gmail.com>
2019-10-10 13:32:57 +0200

    Test if mutation is positionaly passed to federated schema and accessible

--- integration_tests/service_b/src/schema.py
+++ integration_tests/service_b/src/schema.py
@@ -1,3 +1,4 @@
+import graphene
 from graphene import ObjectType, String, Int, Field, Interface
 from graphene_federation import build_schema, key
 
@@ -7,23 +8,23 @@ class TextInterface(Interface):
     body = String(required=True)
 
 
-@key(fields='id')
+@key(fields="id")
 class FunnyText(ObjectType):
     class Meta:
         interfaces = (TextInterface,)
 
     def __resolve_reference(self, info, **kwargs):
-        return FunnyText(id=self.id, body=f'funny_text_{self.id}')
+        return FunnyText(id=self.id, body=f"funny_text_{self.id}")
 
 
-@key(fields='id')
+@key(fields="id")
 class FileNode(ObjectType):
     id = Int(required=True)
     name = String(required=True)
 
     def __resolve_reference(self, info, **kwargs):
         # todo test raise exception here
-        return FileNode(id=self.id, name=f'file_{self.id}')
+        return FileNode(id=self.id, name=f"file_{self.id}")
 
 
 # to test that @key applied only to FileNode, but not to FileNodeAnother
@@ -36,4 +37,16 @@ class Query(ObjectType):
     file = Field(lambda: FileNode)
 
 
-schema = build_schema(Query, types=[FileNode, FunnyText, FileNodeAnother])
+class FunnyMutation(graphene.Mutation):
+    result = String(required=True)
+
+    @classmethod
+    def mutate(cls, root, info, **data):
+        return FunnyMutation(result="Funny")
+
+
+class Mutation(ObjectType):
+    funny_mutation = FunnyMutation.Field()
+
+
+schema = build_schema(Query, Mutation, types=[FileNode, FunnyText, FileNodeAnother])
--- integration_tests/tests/tests/test_main.py
+++ integration_tests/tests/tests/test_main.py
@@ -4,25 +4,22 @@ import requests
 
 def test_integrate_simple_schema():
     query = {
-        'query': """
+        "query": """
             query {
                 goodbye
             }
         """,
-        'variables': {}
+        "variables": {},
     }
-    response = requests.post(
-        'http://federation:3000/graphql/',
-        json=query,
-    )
+    response = requests.post("http://federation:3000/graphql/", json=query)
     assert response.status_code == 200
-    data = json.loads(response.content)['data']
-    assert data['goodbye'] == "See ya!"
+    data = json.loads(response.content)["data"]
+    assert data["goodbye"] == "See ya!"
 
 
 def test_external_types():
     query = {
-        'query': """
+        "query": """
             query {
                 posts {
                     title
@@ -37,39 +34,53 @@ def test_external_types():
                 }
             }
         """,
-        'variables': {}
+        "variables": {},
     }
-    response = requests.post(
-        'http://federation:3000/graphql/',
-        json=query,
-    )
+    response = requests.post("http://federation:3000/graphql/", json=query)
     assert response.status_code == 200
-    posts = json.loads(response.content)['data']['posts']
+    posts = json.loads(response.content)["data"]["posts"]
     assert 3 == len(posts)
-    assert [{"id": 1, "name": "file_1"}] == posts[0]['files']
-    assert {"id": 1, "body": "funny_text_1"} == posts[0]['text']
-    assert [{"id": 2, "name": "file_2"}, {"id": 3, "name": "file_3"}] == posts[1]['files']
-    assert {"id": 2, "body": "funny_text_2"} == posts[1]['text']
-    assert posts[2]['files'] is None
-    assert {"id": 3, "body": "funny_text_3"} == posts[2]['text']
+    assert [{"id": 1, "name": "file_1"}] == posts[0]["files"]
+    assert {"id": 1, "body": "funny_text_1"} == posts[0]["text"]
+    assert [{"id": 2, "name": "file_2"}, {"id": 3, "name": "file_3"}] == posts[1][
+        "files"
+    ]
+    assert {"id": 2, "body": "funny_text_2"} == posts[1]["text"]
+    assert posts[2]["files"] is None
+    assert {"id": 3, "body": "funny_text_3"} == posts[2]["text"]
 
 
 def test_key_decorator_applied_by_exact_match_only():
     query = {
-        'query': """
+        "query": """
             query {
                 _service {
                     sdl
                 }
             }
         """,
-        'variables': {}
+        "variables": {},
     }
-    response = requests.post(
-        'http://service_b:5000/graphql',
-        json=query,
-    )
+    response = requests.post("http://service_b:5000/graphql", json=query)
     assert response.status_code == 200
-    sdl = response.json()['data']['_service']['sdl']
+    sdl = response.json()["data"]["_service"]["sdl"]
     assert 'type FileNode  @key(fields: "id")' in sdl
     assert 'type FileNodeAnother  @key(fields: "id")' not in sdl
+
+
+def test_mutation_is_accessible_in_federation():
+    # this mutation is created in service_b
+    mutation = """
+    mutation {
+        funnyMutation {
+            result
+        }
+    }"""
+
+    response = requests.post(
+        "http://federation:3000/graphql/", json={"query": mutation}
+    )
+    assert response.status_code == 200
+    assert "errors" not in response.json()
+    assert response.json()["data"]["funnyMutation"]["result"] == "Funny"
+
