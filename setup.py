c35fc8bd010a182db363f895b06edba1eac407ad
Igor Kasianov <super.hang.glider@gmail.com>
2019-10-16 23:08:46 +0300

    get rid of download_url

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
   author = 'Igor Kasianov',
   author_email = 'super.hang.glider@gmail.com',
   url = 'https://github.com/erebus1/graphene-federation',
-  download_url = 'https://github.com/erebus1/graphene-federation/archive/0.0.3.tar.gz',
   keywords = ['graphene', 'gql', 'federation'],
   install_requires=[
           "graphene>=2.1.0,<3"
