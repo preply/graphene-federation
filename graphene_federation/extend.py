ae82b9b1d3c22d48ac8f7d60148db56a1bd05bd3
Igor Kasianov <super.hang.glider@gmail.com>
2019-11-20 23:00:10 +0200

    minor refactoring

--- graphene_federation/extend.py
+++ graphene_federation/extend.py
@@ -8,10 +8,10 @@ def register_extend_type(typename, Type):
 
 def extend(fields: str):
     def decorator(Type):
-        register_entity(Type.__name__, Type)
-        register_extend_type(Type.__name__, Type)
         if hasattr(Type, '_sdl'):
             raise RuntimeError("Can't extend type which is already extended or has @key")
+        register_entity(Type.__name__, Type)
+        register_extend_type(Type.__name__, Type)
         setattr(Type, '_sdl', '@key(fields: "%s")' % fields)
         return Type
     return decorator
