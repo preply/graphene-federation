4223953c1047439acdca0055585a7b4f3f72b3ef
Kyrylo Tarasenko <kyrylo.t@preply.com>
2022-07-08 13:41:10 +0300

    upd lib version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 def read(*rnames):
   return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.1.0'
+version = '0.2.0'
 
 setup(
   name = 'graphene-federation',
